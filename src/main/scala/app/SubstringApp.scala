2659760e58f711e46da17b23aea491243f28afcb
Maciej Janusz <maciej.st.janusz@gmail.com>
2016-01-08 02:41:29 +0100

    testing version

--- src/main/scala/app/SubstringApp.scala
+++ src/main/scala/app/SubstringApp.scala
@@ -6,7 +6,6 @@ import akka.actor.{ActorSystem, Props}
 import org.apache.commons.io.FileUtils
 
 object SubstringApp {
-
   val nChunks = 4
   val textfile = FileUtils.readFileToString(new File("400m.txt"))
   val sub = "V3zbkLszG2DFIR0U5IyE"
@@ -52,37 +51,4 @@ object SubstringApp {
       new BlockingStringSearchRunner(repeats).search(sub, input)
     }
   }
-
-  //  def main(args: Array[String]): Unit = {
-  //    val mode = args(0)
-  //    val sub = args(1)
-  //    val filepath = args(2)
-  //    val nChunks = args(3).toInt
-  //    val repeats = args(4).toInt
-  //    val n = args(5).toInt
-  //    val file = new File(filepath)
-  //    val input = FileUtils.readFileToString(file).substring(0, n)
-  //    val chunkSize = input.length / nChunks
-  //    val chunkExtra = sub.length - 1
-  //
-  //    val chunks = for (i <- 0 until nChunks)
-  //      yield {
-  //        val subStart = i * chunkSize
-  //        val end = subStart + chunkSize + chunkExtra
-  //        val subEnd =
-  //          if (i == nChunks - 1 || end > input.length) input.length
-  //          else end
-  //        input.substring(subStart, subEnd)
-  //      }
-  //
-  //    println("N = " + (input.length/1000000) + "mln, M = " + sub.length + ", repeats = " + repeats)
-  //
-  //    if (mode.equals("a")) {
-  //      val actorSystem = ActorSystem()
-  //      val runner = actorSystem.actorOf(Props(classOf[ConcurrentStringSearchRunner], repeats))
-  //      runner ! Run(sub, chunks)
-  //    } else {
-  //      new BlockingStringSearchRunner(repeats).search(sub, input)
-  //    }
-  //  }
 }
